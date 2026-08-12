2af989389edfe2db6e397cb86994d154e7e89566
asheesh <user.asheesh01@gmail.com>
2026-08-12 16:48:19 +0000

    test(calc): add simple test for calc module

---
+++ app/calc.py
@@ -0,0 +1,31 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int) -> int:
+    """Add two numbers.
+
+    Args:
+        x(int): first number
+        y(int): second number
+
+    Returns:
+        int: sum of two numbers
+
+    """
+    return x + y
+
+
+def subtract(x: int, y: int) -> int:
+    """
+    Subtract two numbers.
+
+    Args:
+        x(int): first number
+        y(int): second number
+
+    Returns:
+        int: subtraction of two numbers
+    """
+    return y - x
