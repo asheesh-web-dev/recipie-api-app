7959a940c967cfcc9368d749495cd7be00dc51cf
asheesh <user.asheesh01@gmail.com>
2026-08-25 18:08:47 +0000

    feat(user): normalize the user email

--- core/models.py
+++ core/models.py
@@ -15,7 +15,7 @@ class UserManager(BaseUserManager["User"]):
 
     def create_user(self, email: str, password: str, **extra_fields: object):
         """Create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
