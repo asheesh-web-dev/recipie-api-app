b49c59dc9f6b769a110b097d51e2ab402702cd7f
asheesh <user.asheesh01@gmail.com>
2026-08-25 17:54:10 +0000

    feat(model): add cutom User Model  & UserManager.

---
+++ core/models.py
@@ -0,0 +1,35 @@
+"""
+Database models.
+"""
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.db import models
+
+
+class UserManager(BaseUserManager["User"]):
+    """Manager for users."""
+
+    def create_user(self, email: str, password: str, **extra_fields: object):
+        """Create, save and return a new user."""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
