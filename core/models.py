b1eccf25fe8de6470a74a7e24fa9f033eb353b69
asheesh <user.asheesh01@gmail.com>
2026-09-03 20:21:24 +0000

    feat(model): add superuser functionality

--- core/models.py
+++ core/models.py
@@ -21,6 +21,15 @@ class UserManager(BaseUserManager["User"]):
 
         return user
 
+    def create_superuser(self, email: str, password: str) -> "User":
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
